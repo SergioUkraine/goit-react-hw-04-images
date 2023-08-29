b81ea7993dd9ed18b3936790536fc0ca496efdd6
Serhii <sysoev.khai@gmail.com>
2023-08-30 01:01:02 +0300

    added modal style

--- src/components/Modal/Modal.styled.jsx
+++ src/components/Modal/Modal.styled.jsx
@@ -6,7 +6,8 @@ export const Backdrop = styled.div`
   left: 0;
   width: 100vw;
   height: 100vh;
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: rgba(0, 0, 0, 0.8);
+  z-index: 1200;
 `;
 
 export const ModalContent = styled.div`
@@ -14,14 +15,9 @@ export const ModalContent = styled.div`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  min-height: 300px;
-  max-width: 600px;
-  width: 100%;
-  padding: 12px;
-  background-color: #fff;
-  border-radius: 3px;
-  box-shadow: 0px 2px 1px -1px rgba (0, 0, 0, 0.2),
-    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
 `;
 
-export const ModalImage = styled.img``;
+export const ModalImage = styled.img`
+  max-width: calc(100vw - 48px);
+  max-height: calc(100vh - 24px);
+`;
